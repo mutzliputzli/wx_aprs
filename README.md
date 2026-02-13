fe0229db574d8ec142ab0cf03ea7269681823221
Michael Richter <godwana@gmail.com>
2026-02-13 11:49:33 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wx_aprs
 Python to forward local weather data from Wunderground to the APRS Network.
+
+
+Wunderground API documentation: https://twcapi.co/v2PWSO
+Get APRS passcode: https://www.darc.de/der-club/distrikte/o/ortsverbaende/aprspasscode/
---
+++ aprs_beacon.py
@@ -0,0 +1,101 @@
+﻿# aprs_beacon.py
+
+import time
+import sys
+import json
+import aprslib
+import datetime
+from datetime import datetime
+import pip._vendor.requests
+from pip._vendor import requests
+
+# load properties
+def load_properties(filepath):
+    props={}
+    with open(filepath, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith("#") or line.startswith("!"):
+                continue
+            if "=" in line:
+                key, value = line.split("=", 1)
+                props[key.strip()] = value.strip()
+    return props
+
+props = load_properties("aprs_beacon.properties")
+
+print(props)
+
+wundergroundStationId = props["wundergroundStationId"]
+wundergroundApiKey = props["wundergroundApiKey"]
+aprsCallsign = props["aprsCallsign"]
+aprsPasscode = props["aprsPasscode"]
+
+
+# get weather data from Wunderground as json
+url = "https://api.weather.com/v2/pws/observations/current?stationId="+wundergroundStationId+"&format=json&units=e&apiKey="+wundergroundApiKey
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = json.loads(response.text)
+    print(data)
+else:
+    print(f"Error retrieving data, status code: {response.status_code}")
+
+# get date from request
+temperature = data['observations'][0]['imperial']['temp']
+humidity = data['observations'][0]['humidity']
+pressure = data['observations'][0]['imperial']['pressure']
+wind_direction = data['observations'][0]['winddir']
+wind_speed = data['observations'][0]['imperial']['windSpeed']
+rainfall_since_midnight = data['observations'][0]['imperial']['precipTotal']
+lon = data['observations'][0]['lon']
+lat = data['observations'][0]['lat']
+
+print("temperature", temperature)
+print("humidity", humidity)
+print("pressure", pressure)
+print("wind_direction", wind_direction)
+print("wind_speed", wind_speed)
+print("rain_since_midnight", rainfall_since_midnight)
+print("lon", lon)
+print("lat", lat)
+
+# converting pressure from inHg to mbar
+def inhg_to_mbar(inhg_value:float) -> float:
+    return inhg_value * 33.8638866667
+
+pressure = inhg_to_mbar(pressure)
+
+# formatting weather data
+temperaturestr = str("%03d" % temperature)
+humiditystr = str ("%02d" %humidity)
+pressurestr = str ("%05d" %pressure)
+wind_directionstr = str ("%03d" % wind_direction)
+wind_speedstr = str ("%03d" % wind_speed)
+rainfall_since_midnightstr = str ("%03d" %rainfall_since_midnight)
+
+print("temperaturestr", temperaturestr)
+print("humiditystr", humiditystr)
+print("pressurestr", pressurestr)
+print("wind_directionstr", wind_directionstr)
+print("wind_speedstr", wind_speedstr)
+print("rainfall_since_midnightstr", rainfall_since_midnightstr)
+
+# convert time to UTC
+UTC = datetime.utcnow()
+formatted_utc = UTC.strftime('%H%M%S')
+print("formatted_utc", formatted_utc)
+
+# my fixed coords for now, could be converted from wunderground lon/lat data
+coords = "5041.35N/00710.98E"
+
+# combine APRS payload
+APRS_String  = aprsCallsign+">APRS,TCPIP*:@"+formatted_utc+"z"+coords+"_"+wind_directionstr+"/"+wind_speedstr+"t"+temperaturestr+"P"+rainfall_since_midnightstr+"h"+humiditystr+"b"+pressurestr+"powered by wunderground.com"
+
+# send via TCPIP on port 14580 with appropriate passcode
+#AIS = aprslib.IS(aprsCallsign, aprsPasscode, port=14580)
+#AIS.connect()
+#AIS.sendall(APRS_String)
+
+print(APRS_String)
